a2b7d144bd35eb566f951b7d4a84266169014ea0
Harris <tc.harris@ubc.ca>
2025-11-13 16:35:46 -0800

    adding Ung 2008 data

---
+++ R/UNG_1.R
@@ -0,0 +1,12 @@
+#' Coefficients for allometric equations for DBH
+#'
+#' @format
+#' A data frame with 136 rows and 4 columns:
+#' \describe{
+#'   \item{SPECIES}{Species codes. Follows NFI naming conventions}
+#'   \item{COMPONENT}{Part of the tree}
+#'   \item{BETA_NUM}{Corrresponds to beta coefficents}
+#'   \item{VALUES}{Values}
+#' }
+#' @source Values from Ung et al., 2008 Canadian national biomass equations: new parameter estimates that include British Columbia data
+"UNG_1"
