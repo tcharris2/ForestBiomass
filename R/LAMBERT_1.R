aa007b8d5c247d853d51f06be784fee02f07e0a3
Harris <tc.harris@ubc.ca>
2025-11-13 16:29:16 -0800

    adding Lambert 2005 data

---
+++ R/LAMBERT_1.R
@@ -0,0 +1,12 @@
+#' Coefficients for allometric equations for DBH only
+#'
+#' @format
+#' A data frame with 288 rows and 4 columns:
+#' \describe{
+#'   \item{SPECIES}{Species codes. Follows NFI naming conventions}
+#'   \item{COMPONENT}{Part of the tree}
+#'   \item{BETA_NUM}{Corrresponds to beta coefficents}
+#'   \item{VALUES}{Values}
+#' }
+#' @source Values from Lambert et al., 2005 Canadian national tree aboveground biomass equations
+"LAMBERT_1"
---
+++ R/LAMBERT_2.R
@@ -0,0 +1,12 @@
+#' Coefficients for allometric equations for DBH and HEIGHT
+#'
+#' @format
+#' A data frame with 432 rows and 4 columns:
+#' \describe{
+#'   \item{SPECIES}{Species codes. Follows NFI naming conventions}
+#'   \item{COMPONENT}{Part of the tree}
+#'   \item{BETA_NUM}{Corrresponds to beta coefficents}
+#'   \item{VALUES}{Values}
+#' }
+#' @source Values from Lambert et al., 2005 Canadian national tree aboveground biomass equations
+"LAMBERT_2"
